2d026c2bd2bcdbb41a21296692533ea21ceea71c
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-08 17:49:00 +0200

    working on lamp button

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -10,5 +10,5 @@ let buttonEl = document.getElementById('button');
 
 buttonEl.addEventListener('click', ()=> {
 
-    imgEl.classList.add('light').src="yellow_lamp"
+    imgEl.classList.add('light').src="./assets/img/yellow_lamp.png"
 })
