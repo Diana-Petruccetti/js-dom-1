750192397263dc3d1c52903796e964d5d0b5a01a
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-08 17:21:01 +0200

    working on js

---
+++ assets/javascript/index.js
@@ -0,0 +1,10 @@
+
+// Quando clicchi il pulsante la lampadina si accende
+// e cambia la scritta da accesa a spenta e viceversa.
+
+// Fase 1: seleziono gli elementi della DOM
+let imgEl = document.getElementById('lamp');
+let buttonEl = document.getElementById('button');
+
+// Fase 2: collego un event listener all'immagine e al pulsante
+
